b3d9f31379b51b88fcaa06d99d9fd022e3a5f3cc
AyushRay06 <rayayush47@gmail.com>
2024-04-18 17:49:43 +0530

    route-init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image"
+import axios from "axios"
 
-export default function Home() {
-  return <div>HI There</div>
+async function getUserDetails() {
+  try {
+    const response = await axios.get("http://localhost:3000/api/user")
+    return response.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export default async function Home() {
+  const userData = await getUserDetails()
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.name}</div>
+
+          {userData?.email}
+        </div>
+      </div>
+    </div>
+  )
 }

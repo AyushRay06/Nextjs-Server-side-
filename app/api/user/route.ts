f378a36a048895159ffa448fd0275b88fda67697
AyushRay06 <rayayush47@gmail.com>
2024-04-25 02:45:18 +0530

    POST-endpoint

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,6 +4,7 @@ export async function GET(req: NextResponse) {
   return NextResponse.json({ name: "Ayush", email: "rayayush@gmail.com" })
 }
 
-export async function POST(rer: NextResponse, res: NextResponse) {
-  return NextResponse.json({ msg: "Done" })
+export async function POST(req: NextResponse, res: NextResponse) {
+  const body = await req.json()
+  return NextResponse.json({ username: "username", password: "password" })
 }
